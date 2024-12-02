03ca54e170f732c20e4856b5008c56364124e8ed
itssiem <siem@siemgerritsen.nl>
2024-12-02 17:24:10 +0100

    initial commit

---
+++ 2024/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn main() {
+    let file_path = "inputs/01";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    let mut a = Vec::<i32>::new();
+    let mut b = Vec::<i32>::new();
+    let lines = contents.lines();
+    for line in lines {
+        let entries = line.split("   ").collect::<Vec<&str>>();
+
+        a.push(entries[0].parse::<i32>().unwrap());
+        b.push(entries[1].parse::<i32>().unwrap());
+    }
+    a.sort();
+    b.sort();
+
+    let mut sum = 0;
+    let mut i = 0;
+    while i < a.len() {
+        sum += (a[i] - b[i]).abs();
+        i += 1;
+    }
+    println!("{}", sum);
+}
