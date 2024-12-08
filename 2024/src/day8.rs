10048ad6d6e689d4875469b34aaf087afdc42859
itssiem <siem@siemgerritsen.nl>
2024-12-08 23:26:53 +0100

    added day 8 part 2

--- 2024/src/day8.rs
+++ 2024/src/day8.rs
@@ -71,6 +71,59 @@ fn draw_charmap(charmap: Vec<Vec<char>>) {
 }
 
 #[aoc(day8, part2)]
-fn part2(input: &str) -> String {
-    todo!()
+fn part2(input: &str) -> i32 {
+    let mut charmap = input.lines().collect::<Vec<&str>>()
+        .into_iter().map(|s| {s.chars().collect::<Vec<char>>()})
+        .collect::<Vec<Vec<char>>>();
+
+    let mut antennas: HashMap<char, Vec<Position>> = charmap.clone()
+        .into_iter()
+        .flatten()
+        .filter(|c| *c != '.')
+        .map(|c| (c, vec!()))
+        .collect();
+
+    for y in 0..charmap.len() {
+        for x in 0..charmap[0].len() {
+            if antennas.contains_key(&charmap[y][x]) {
+                antennas.get_mut(&charmap[y][x])
+                    .expect("No antenna found")
+                    .push(Position { x: x as i32, y: y as i32});
+            }
+        }
+    }
+
+    let mut antinodes = 0;
+    let mut antinode_positions: HashSet<Position> = HashSet::new();
+    for freq in antennas.keys() {
+        for a in &antennas[freq] {
+            for b in &antennas[freq] {
+                let distance_sq = (a.x - b.x).pow(2) * (a.y - b.y).pow(2);
+                if distance_sq == 0 {
+                    continue;
+                }
+                let distance_x = a.x - b.x;
+                let distance_y = a.y - b.y;
+                let mut antinode_x = a.x;
+                let mut antinode_y = a.y;
+
+                while antinode_x < charmap[0].len() as i32 && 
+                    antinode_x >= 0 &&
+                    antinode_y < charmap.len() as i32 &&
+                    antinode_y >= 0 {
+                    if !antinode_positions.contains(&Position {x: antinode_x, y: antinode_y}) {
+                        antinodes += 1;
+                        //charmap[antinode_y as usize][antinode_x as usize] = '#';
+                        antinode_positions.insert(Position {x: antinode_x, y: antinode_y});
+                    }
+                    antinode_x += distance_x;
+                    antinode_y += distance_y;
+                }
+
+            }
+        }
+    }
+    //draw_charmap(charmap);
+
+    return antinodes;
 }
