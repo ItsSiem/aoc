e98882fef47f8dc01791e95ea5c46aef9e2adf49
itssiem <siem@siemgerritsen.nl>
2024-12-09 22:50:46 +0100

    day 9 part 2

--- 2024/src/day9.rs
+++ 2024/src/day9.rs
@@ -51,6 +51,75 @@ fn part1(input: &str) -> u64 {
 }
 
 #[aoc(day9, part2)]
-fn part2(input: &str) -> String {
-    todo!()
+fn part2(input: &str) -> u64 {
+    let mut disk: Vec<Vec<String>> = vec!();
+    
+    let mut index = 0;
+    let mut id = 0;
+    for size in input.chars().map(|c| c.to_digit(10).unwrap()) {
+        let mut s = ".".to_string();
+        if index % 2 == 0 {
+            s = id.to_string();
+            id += 1;
+        }
+        let mut v: Vec<String> = vec!();
+        for _ in 0..size {
+            v.push(s.clone());
+        }
+        if v.len() != 0 {
+            disk.push(v);
+        }
+        index += 1;
+    }
+
+    // println!("{}", disk.clone().into_iter().flatten().collect::<Vec<String>>().join(""));
+
+    let mut i = (disk.len() - 1) as i32;
+    while i > 0 {
+        let mut j = 0;
+        while disk[i as usize][0] == ".".to_string() {
+            i -= 1;
+        }
+        while !(disk[j][0] == ".".to_string() && disk[j].len() >= disk[i as usize].len()) {
+            j += 1;
+            if j >= disk.len() {
+                break;
+            }
+        }
+        if j > i as usize {
+            i -= 1;
+            continue;
+        }
+        //println!("i: {:?}, j:{:?}", disk[i as usize], disk[j]);
+        if disk[j].len() == disk[i as usize].len() {
+            disk[j] = disk[i as usize].clone();
+        disk[i as usize] = vec!(".".to_string(); disk[i as usize].len()); 
+        }
+        else {
+            let remainder: i32 = disk[j].len() as i32 - disk[i as usize].len() as i32;
+            if remainder < 0 {
+                println!("something is horribly wrong");
+                break;
+            }
+            disk[j] = disk[i as usize].clone();
+            disk[i as usize] = vec!(".".to_string(); disk[i as usize].len()); 
+            disk.insert(j + 1, vec!(".".to_string(); remainder as usize));
+        }
+    }
+
+    //println!("{}", disk.clone().into_iter().flatten().collect::<Vec<String>>().join(""));
+
+    let mut checksum = 0;
+    let mut i = 0;
+    for f in disk.clone()
+        .into_iter()
+        .flatten()
+        .collect::<Vec<String>>() {
+        if !f.contains(".") {
+            checksum += f.parse::<u64>().unwrap() * i;
+        }
+        i += 1;
+    }
+
+    return checksum;
 }
